4f76518346a483f0d46d5afd34fc6ecdb77596b6
Danilo Calegaro <danilo.calegaro@hotmail.com>
2022-07-29 09:00:40 -0300

    feat: acrescentanto titulo na página

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app'
+import Head from 'next/head'
 import { Copyright } from '../components/Copyright'
 import { Footer } from '../components/Footer'
 import { Header } from '../components/Header'
@@ -8,6 +9,9 @@ import GlobalStyles from '../styles/global'
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <Head>
+        <title>Danilo - Developer</title>
+      </Head>
       <GlobalStyles />
       <Header />
       <Component {...pageProps} />
